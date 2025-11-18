0b30be31f4b44a782ec582d35f604bd539a7b421
ILGiXO <ksil3651@gmail.com>
2025-11-18 09:43:35 +0900

    1. 사용자 회원관리, 2. 관리자 운동 가이드생성 테스트 케이스 커밋

--- resoucres/testcase/testcase.sql
+++ resoucres/testcase/testcase.sql
@@ -1,3 +1,55 @@
+# 1. 사용자 회원가입
+-- 1) 회원가입
+INSERT INTO Mem (user_id, user_pw, user_name, user_nickname, user_email, user_join_date ,user_phone, user_height, user_weight)
+VALUES('userA',
+       'pw1',
+       '사용자',
+       'userA',
+       'userA@email.com',
+       NOW(),
+       '010-1111-1111',
+       170.5,
+       65.2);
+
+-- 2) 회원가입 조회(회원정보 조회, 로그인 성공)
+SELECT * FROM mem WHERE user_id = 'userA' AND user_pw = 'pw1';
+
+-- 3) 마이페이지로 이동하여 닉네임을 '운동초보A'로 변경
+UPDATE Mem
+SET user_nickname ='운동초보A'
+where user_num = 34;
+
+-- 4)회원탈퇴(회원 탈퇴 진행)
+--      (0) 회원 탈퇴
+DELETE FROM Mem WHERE user_num = 36 ;
+--      (1) 회원 탈퇴 확인
+SELECT * FROM mem;
+
+#2 관리자 운동 가이드 생성
+-- 관리자 계정으로 관리자 페이지에 로그인
+-- 1)관리자 회원가입
+INSERT INTO Adm (adm_id, adm_pw, adm_name)
+VALUES ('admin1',
+        'pw1',
+        '관리자1');
+
+-- 2) 관리자 확인
+SELECT * FROM adm;
+
+-- 3)콘텐츠 관리 초보자용 운동 가이드 등록
+INSERT INTO ExerciseGuide (adm_no, guide_title, guide_video, guide_image, guide_des, guide_date, ex_num)
+VALUES(1,
+       '벤치프레스 가이드',
+       'https://youtube.com/bench',
+       'https://img.com/bench.png',
+       '벤치프레스 자세',
+       NOW(),
+       1);
+
+
+-- 4)운동 가이드 목록에 ' 초보자용 스쿼트가 정상적으로 추가되었는지 확인합니다.
+SELECT * FROM ExerciseGuide;
+
 # 3. 사용자 로그인 -> 운동 가이드
 start transaction;
 -- 1) 로그인
